fb364f16710d8fe5213dfa6083f1f3f69c43bc8b
meihengshuo <73676296+meihengshuo@users.noreply.github.com>
2022-08-25 15:38:24 +0800

    Initial commit

---
+++ gulimall-order/src/test/java/com/mhs/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhs.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
